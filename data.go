2d73f8e509abe8e17da3a39f4c4c14d46ef1cef2
Elijah Morgan <elimorga@cisco.com>
2024-01-25 12:55:02 -0500

    Remove Company struct

--- data.go
+++ data.go
@@ -6,22 +6,9 @@ import (
 )
 
 var (
-	data    []Company
 	devices []Device
 
 )
-
-type StateEnum struct {
-	State   string
-	Display string
-}
-
-type Company struct {
-	ID      string
-	Company string
-	Contact string
-	Country string
-}
 
 type Device struct {
 	ID                 string
@@ -39,26 +26,6 @@ type DeviceState struct {
 }
 
 func init() {
-	data = []Company{
-		{
-			ID:      "1",
-			Company: "Amazon",
-			Contact: "Jeff Bezos",
-			Country: "United States",
-		},
-		{
-			ID:      "2",
-			Company: "Apple",
-			Contact: "Tim Cook",
-			Country: "United States",
-		},
-		{
-			ID:      "3",
-			Company: "Microsoft",
-			Contact: "Satya Nadella",
-			Country: "United States",
-		},
-	}
 	devices = []Device{
 		{
 			ID:        "1",
@@ -85,59 +52,6 @@ func init() {
 			Notes:     "under maintenance",
 		},
 	}
-}
-
-func getCompanyByID(id string) Company {
-	var result Company
-	for _, i := range data {
-		if i.ID == id {
-			result = i
-			break
-		}
-	}
-	return result
-}
-
-func updateCompany(company Company) {
-	result := []Company{}
-	for _, i := range data {
-		if i.ID == company.ID {
-			i.Company = company.Company
-			i.Contact = company.Contact
-			i.Country = company.Country
-		}
-		result = append(result, i)
-	}
-	data = result
-}
-
-func addCompany(company Company) {
-	max := 0
-	for _, i := range data {
-		n, _ := strconv.Atoi(i.ID)
-		if n > max {
-			max = n
-		}
-	}
-	max++
-	id := strconv.Itoa(max)
-
-	data = append(data, Company{
-		ID:      id,
-		Company: company.Company,
-		Contact: company.Contact,
-		Country: company.Country,
-	})
-}
-
-func deleteCompany(id string) {
-	result := []Company{}
-	for _, i := range data {
-		if i.ID != id {
-			result = append(result, i)
-		}
-	}
-	data = result
 }
 
 func getDeviceByID(id string) Device {
